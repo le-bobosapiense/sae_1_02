563e3fc267364d2771642786a5df19d3d49750a7
le-bobosapiense <159957947+le-bobosapiense@users.noreply.github.com>
2025-12-14 16:29:56 +0100

    Add files via upload

--- sae_1_02_moi.c
+++ sae_1_02_moi.c
@@ -24,15 +24,10 @@ int ram(struct Arbre arbres[]) {
 
     char line[256];         // capacité de lecture de caractère par ligne ici 256
     int nb_arbres = 0;      // compteur du nombre d'arbres chargés
-    int ligne_num = 0;      // compteur pour savoir si on est sur la première ligne (en-têtes)
-
     
     while (fgets(line, sizeof(line), f)) {      // Boucle pour lire le fichier ligne par ligne
         line[strcspn(line, "\n")] = '\0';  // retirer le caractère '\n' à la fin de la ligne
-        if (ligne_num == 0) {              // si c'est la première ligne en-tête
-            ligne_num++;                        // on l'incrémente
-            continue;                       // on passe à la ligne suivante
-        }
+
 
         char *token = strtok(line, ";");    // découper la ligne en tokens (séparés par ';')
         struct Arbre a;                     // créer une variable temporaire pour stocker l'arbre
@@ -56,7 +51,6 @@ int ram(struct Arbre arbres[]) {
 
         arbres[nb_arbres] = a; // stocker l'arbre dans le tableau
         nb_arbres++;           // incrémenter le nombre d'arbres stockés
-        ligne_num++;           // passer à la ligne suivante
     }
 
     fclose(f);                  // fermer le fichier
@@ -80,79 +74,39 @@ void afficher_RAM(struct Arbre arbres[], int nb_arbres) {
 }
 
 
+void rechercher_RAM(struct Arbre arbres[], int nb_arbres) {
+    char arbre_cherche[20];
+    printf("Choisir la catégorie d'arbre que vous voulez rechercher : ");
+    scanf("%19s", arbre_cherche);
 
+    int trouve = 0; // pour savoir si on a trouvé au moins un arbre
 
-
-
-int aficher(){
-    FILE *f = fopen("foret_arbres_50_V1.csv", "r");      // ouverture du fichier css situer dans le même dossier que l'exécutable
-    if (!f) {
-        perror("Erreur ouverture fichier");     //si on arrive pas a ouvrire le fichier
-        return 1;
-    }
-    char line[256];                             // capacité de lecture de caractère par ligne
-    while (fgets(line, sizeof(line), f)) {      // lie le fichier f ligne par ligne; 
-        line[strcspn(line, "\n")] = '\0';       //permet de suprimer le retour a la ligne automatique a la fin du dernier token de chaque ligne
-
-        char *token = strtok(line, ";");      // découpe la ligne par token avec comme séparateur ;
-        
-        while (token != NULL) {
-            printf("| %-12s ", token);          // affiche le fichier f ligne par ligne
-            token = strtok(NULL, ";"); // passer au token suivant
+    for (int i = 0; i < nb_arbres; i++) {
+        if (strcmp(arbres[i].Espece, arbre_cherche) == 0) {
+            printf("| %-12s | %-12s | %3d | %6.2f | %6.2f | %6.2f | %2d |\n",
+                   arbres[i].Identifiant,
+                   arbres[i].Espece,
+                   arbres[i].Age,
+                   arbres[i].Hauteur,
+                   arbres[i].Diametre,
+                   arbres[i].Volume,
+                   arbres[i].Sante);
+            trouve = 1;
         }
-        printf("|\n");
-        printf("------------------------------------------------------------------------------------------------------\n");       // pour séparer les ligne
-
     }
+
+    if (!trouve) {
+        printf("Aucun arbre de l'espèce '%s' trouvé.\n", arbre_cherche);
+    }
+
+    printf("-------------------------------------------------------------------------------\n");
 }
 
-int rechercher(void) {
-    FILE *f = fopen("foret_arbres_50_V1.csv", "r");      // ouverture du fichier css situer dans le même dossier que l'exécutable
-    if (!f) {
-        perror("Erreur ouverture fichier");     //si on arrive pas a ouvrire le fichier
-        return 1;
-    }
-
-    char line[256];         // capacité de lecture de caractère par ligne
-    char copy_line[256];    // copi de line pour avoir la ligne intacte
-
-    int colonne_espece = -1;  // position de la colonne Espece
-    int ligne_num = 0;        // compteur de lignes
-
-    while (fgets(line, sizeof(line), f)) {  // lie le fichier f ligne par ligne; 
-        //boucle : fgets lit une ligne entière du fichier f dans la mémoire line, elle lit jusqu’au prochain saut de ligne (\n) ou jusqu’à 
-        //ce que line soit pleine (ici 256 caractères max). La boucle while continue tant qu’il y a des lignes à lire. 
-        
-        line[strcspn(line, "\n")] = '\0';   //permet de suprimer le retour a la ligne automatique a la fin du dernier token de chaque ligne
-        int colonne = 0;                  // compteur pour les colonnes de cette ligne
-        strcpy (copy_line, line);         // on créer une copy de line car il vas être fragmentée 
-        char *token = strtok(line, ";");      // découpe la ligne par token avec comme séparateur ;
-
-        while (token != NULL) {
-            if (ligne_num == 0) { // première ligne = en-têtes
-                if (strcmp(token, "Espece") == 0) {     //regarde ci le token et égal a Espece en considérent les majuscule
-                    colonne_espece = colonne; // mémoriser la position
-                }
-            } else { // autres lignes = données
-                if (colonne == colonne_espece) {
-                    if (strcmp(token, "Chene") == 0){
-                        printf("%s",copy_line);             // copie la ligne si on a dans la colone Espece, Chene
-                    }
-                }
-            }
-            colonne++;                  // on ajoute 1 a notre compteur de colone
-            token = strtok(NULL, ";"); // passer au token suivant
-        }
-        ligne_num++; // passer à la ligne suivante
-    }
-
-    fclose(f);
-    return 0;
-}
 
 int main() {
-    struct Arbre arbres[60];
+    struct Arbre arbres[61];
     int nb_arbres = ram(arbres);
     afficher_RAM(arbres, nb_arbres);
+    rechercher_RAM(arbres, nb_arbres);
     return 0;
 }
